532dd20842f0db9714f334ceaff4023b3c4bdf7b
Stephan Hennig <sh2d@arcor.de>
2014-01-23 20:00:15 +0100

    example: Pattern driven node list manipulation.
    Mark all valid hyphenation points by colorizing surrounding letters.

---
+++ examples/pdnm/hyph-mark-color/pdnm_hyph-mark-color.lua
@@ -0,0 +1,35 @@
+-- -*- coding: utf-8 -*-
+local unicode = require('unicode')
+
+-- Module table.
+local M = {}
+
+local Nnew = node.new
+local Ninsert_after = node.insert_after
+local Ninsert_before = node.insert_before
+
+local WHATSIT = node.id('whatsit')
+local PDF_COLORSTACK = node.subtype('pdf_colorstack')
+
+local function colorize_spots(head, tnode, tparent, tlevels)
+   for pos, level in ipairs(tlevels) do
+      if (level % 2 == 1) and not tparent[pos-1] and not tparent[pos] then
+         local push = Nnew(WHATSIT, PDF_COLORSTACK)
+         local pop = Nnew(WHATSIT, PDF_COLORSTACK)
+         push.stack = 0
+         pop.stack = 0
+         push.command = 1
+         pop.command = 2
+         push.data = '1 0 0 rg'
+         Ninsert_before(head, tnode[pos-1], push)
+         Ninsert_after(head, tnode[pos], pop)
+      end
+   end
+end
+
+local function manipulation(head, tnode, tparent, tlevels)
+   colorize_spots(head, tnode, tparent, tlevels)
+end
+M.manipulation = manipulation
+
+return M
